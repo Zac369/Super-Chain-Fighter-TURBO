2f7c2d763289fa0b14044fc232a200776ed794d4
Zac Williams <williamszac20@gmail.com>
2022-03-05 14:18:00 +0000

    Base NFT contract

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contract/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import 'base64-sol/base64.sol';
 
 import "hardhat/console.sol";
 
@@ -29,18 +31,52 @@ contract Game is ERC721 {
     function mintCharacterNFT(uint _characterIndex) external {
         _safeMint(msg.sender, tokenCounter + 1);
 
-        nftHolderAttributes[newItemId] = CharacterAttributes({
+        nftHolderAttributes[tokenCounter + 1] = CharacterAttributes({
             characterIndex: _characterIndex,
-            name: defaultCharacters[_characterIndex].name,
-            imageURI: defaultCharacters[_characterIndex].imageURI,
-            hp: defaultCharacters[_characterIndex].hp,
-            maxHp: defaultCharacters[_characterIndex].hp,
-            attackDamage: defaultCharacters[_characterIndex].attackDamage
+            name: allCharacters[_characterIndex].name,
+            imageURI: allCharacters[_characterIndex].imageURI,
+            hp: allCharacters[_characterIndex].hp,
+            attackDamage: allCharacters[_characterIndex].attackDamage
         });
 
         console.log("Minted NFT w/ tokenId %s and characterIndex %s", tokenCounter + 1, _characterIndex);
 
         tokenCounter += 1;
+    }
+
+    function tokenURI(uint tokenId) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[tokenId];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        charAttributes.name,
+                        ' -- NFT #: ',
+                        Strings.toString(tokenId),
+                        '", "description": "An epic NFT", "image": "ipfs://',
+                        charAttributes.imageURI,
+                        '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,'}, { "trait_type": "Attack Damage", "value": ',
+                        strAttackDamage,'} ]}'
+                    )
+                )
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+            
+        return output;
+
+    }
+
+    function getAllCharacters() public view returns (CharacterAttributes[] memory) {
+        return allCharacters;
     }
 
 }
