d401f88a7413c394e2409c8fee3b2d26a36e396e
Zac Williams <williamszac20@gmail.com>
2022-03-06 21:56:19 +0000

    hitbox stuff

--- js/gameScene.js
+++ js/gameScene.js
@@ -15,20 +15,72 @@ class GameScene extends Phaser.Scene {
     };
 
     create () {
+        this.hp = 200;
+
         this.add.image(400, 300, 'sky');
+
+        this.add.text(10, 10, "health");
+
+        //the health bar is drawn here
+        //it's static and doesn't change
+        //there is an event described in phaser-component-health (github) called 'healthchange'
+        //that can be used to make the health bar dynamic
+        this.add.rectangle(200, 18, 200, 20, "0xf54242");
 
         var platforms = this.physics.add.staticGroup();
 
         platforms.create(400, 570, 'ground').setScale(2).refreshBody();
 
-        this.player = this.physics.add.sprite(80, 450, this.selectedCharacter);
+        this.player1 = this.physics.add.sprite(80, 450, this.selectedCharacter);
 
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
+        this.player2 = this.physics.add.sprite(450, 450, 'heavy');
 
-        this.player.body.setGravityY(300);
+        this.player1.setBounce(0.2);
+        this.player1.setCollideWorldBounds(true);
+        this.player2.setCollideWorldBounds(true);
 
-        this.physics.add.collider(this.player, platforms);
+        this.player1.body.setGravityY(300);
+        this.player2.body.setGravityY(300);
+        this.player2.body.setMaxVelocityX(0);
+
+        /**
+         * This is the initial set up for the "hit boxes"
+         * Right now the hit box is the size of the entire character
+         * This should be updated in order to acheive the "Joust" style gameplay
+         * This is where we should add the hit boxes for the fist or foot for attacks
+         * 
+         * **/
+        this.player1.body.setSize(40, 100);
+        this.player2.body.setSize(40, 100);
+
+        this.physics.add.collider(this.player1, platforms);
+        this.physics.add.collider(this.player2, platforms);
+
+        /**
+         * allows characters to collide with each other
+         * the function describes what event should occur upon collision
+         * 
+         * TO DO: punch/kick animation, punch/kick hit boxes, punch/kick damage, punch/kick hit box colliders
+         * 
+         * this is where you will put how much damage P1 can do to P2
+         * this should be dynamically expressed by the NFT character loaded
+         * 
+         * right now P1 kills P2 after colliding with him enough times because no punch/kick parts have been finished
+         * **/
+        this.physics.add.collider(this.player1, this.player2, function (playerOne, playerTwo) {
+            playerTwo.damage(0.1);
+
+        });
+
+            //enemy health described here (starting health, min health, max health);
+        //one can be added for P1 easily and both P1 and P2 should be dynamic with the NFT
+        PhaserHealth.AddTo(this.player2, 2, 0, 2);
+
+        //describes what to do when the die command is executed
+        //can be changed to add a die animation
+        this.player2.on('die', function (spr) {
+            spr.setActive(false).setVisible(false);
+        });
 
         this.anims.create({
             key: 'left',
@@ -50,30 +102,43 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         });
 
+        //this is the punch animation but I haven't drawn any hit sprites yet
+        //right now it's the same as a walk to the right
+        this.anims.create({	
+            key: 'punch',
+            frames: this.anims.generateFrameNumbers(this.selectedCharacter, { start: 5, end: 7 }),
+            frameRate: 20
+        });
+
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        //the button for a punch is currently D, but can be changed
+	    this.punchInput = this.input.keyboard.addKey("D");
     };
 
     update () {
         if (this.cursors.left.isDown)
         {
-            this.player.setVelocityX(-160);
+            this.player1.setVelocityX(-160);
 
-            this.player.anims.play('left', true);
+            this.player1.anims.play('left', true);
         } else if (this.cursors.right.isDown)
         {
-            this.player.setVelocityX(160);
+            this.player1.setVelocityX(160);
 
-            this.player.anims.play('right', true);
+            this.player1.anims.play('right', true);
+        } else if (this.punchInput.isDown) {
+            this.player1.anims.play('punch', true);
         } else
         {
-            this.player.setVelocityX(0);
+            this.player1.setVelocityX(0);
 
-            this.player.anims.play('turn');
+            this.player1.anims.play('turn');
         }
 
-        if (this.cursors.up.isDown && this.player.body.touching.down)
+        if (this.cursors.up.isDown && this.player1.body.touching.down)
         {
-            this.player.setVelocityY(-330);
+            this.player1.setVelocityY(-330);
         }
     };
 }
